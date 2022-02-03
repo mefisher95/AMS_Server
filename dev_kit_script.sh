cbe0d6f586ce7ad5e0d059b0a9eebabb1b4681eb
Michael Fisher <56176130+mefisher95@users.noreply.github.com>
2022-02-02 19:58:54 -0600

    Update dev_kit_script.sh

--- dev_kit_script.sh
+++ dev_kit_script.sh
@@ -5,3 +5,8 @@ sudo apt upgrade -y
 sudo apt install -y g++
 g++ server.cpp -o Server.out
 g++ client.cpp -o Client.out
+
+cd ~
+git clone https://github.com/data61/MP-SPDZ.git
+apt-get install -Y automake build-essential git libboost-dev libboost-thread-dev libntl-dev libsodium-dev libssl-dev libtool m4 python3 texinfo yasm 
+cd AMS_Server
